b2799292359cfc2adb0452ce77e36b9b6ef01a6a
Nishant <nishantsalhotra.ns@gmail.com>
2018-05-04 04:36:13 +0530

    Exercises: Add exercises for promises and classes

---
+++ __tests__/test.js
@@ -0,0 +1,157 @@
+const {
+  timeout,
+  greet,
+  rejectPromise,
+  allPromises,
+  Queue,
+  Person,
+  sequentialPromise,
+} = require('../src');
+
+/* eslint-disable no-unused-vars */
+
+describe('timeout', () => {
+  test('works with promises', () => {
+    timeout('Pesto').then(data => expect(data).toBe('Hello Pesto'));
+  });
+});
+
+describe('greet', () => {
+  test('The function should return a Promise', () => {
+    expect(greet() instanceof Promise).toBe(true);
+  });
+  test('Promise resolve value', () => expect(greet('Pesto')).resolves.toBe('Hey Pesto'));
+});
+
+describe('rejectPromise', () => {
+  test('The function should return a Promise', () => {
+    expect(rejectPromise() instanceof Promise).toBe(true);
+  });
+
+  test('Promise resolve value', () => expect(rejectPromise()).resolves.toBe('REJECTED!'));
+});
+
+describe('allPromises', () => {
+  test('The function should return a Promise', () => {
+    expect(allPromises() instanceof Promise).toBe(true);
+  });
+
+  test('Promise call should return an array of values from promises', () => {
+    const p1 = new Promise(res => res(1));
+    const p2 = 2;
+    const p3 = Promise.resolve(3);
+    return expect(allPromises([p1, p2, p3])).resolves.toEqual([1, 2, 3]);
+  });
+});
+
+describe('Queue', () => {
+  test('should create an object with an empty items array and count 0', () => {
+    const myQueue = new Queue();
+    expect(myQueue).toEqual({
+      items: [],
+      count: 0,
+    });
+  });
+
+  test('should have a method to queue an item to the end of the queue', () => {
+    const myQueue = new Queue();
+    myQueue.queue(1);
+    myQueue.queue(2);
+    myQueue.queue(3);
+    myQueue.queue(4);
+    expect(myQueue.items[3]).toBe(4);
+  });
+
+  test('should increment the property count when item is queued', () => {
+    const myQueue = new Queue();
+    expect(myQueue.count).toBe(0);
+    myQueue.queue(1);
+    expect(myQueue.count).toBe(1);
+    myQueue.queue(2);
+    expect(myQueue.count).toBe(2);
+    myQueue.queue(3);
+    expect(myQueue.count).toBe(3);
+  });
+
+  test('should have a method to dequeue an item from the end of the queue', () => {
+    const myQueue = new Queue();
+    myQueue.queue(1);
+    myQueue.queue(2);
+    myQueue.queue(3);
+    myQueue.dequeue();
+    expect(myQueue.items[1]).toBe(2);
+  });
+
+  test('should decrement the property count when item is dequeued', () => {
+    const myQueue = new Queue();
+    myQueue.queue(1);
+    myQueue.queue(2);
+    myQueue.queue(3);
+    myQueue.dequeue();
+    expect(myQueue.count).toBe(2);
+  });
+});
+
+describe('Person', () => {
+  test('should be a class', () => {
+    expect(/class/gm.test(Person.toString())).toBe(true);
+  });
+
+  test('should create an object with firstName, lastName, dateOfBirth when called with new keyword', () => {
+    const nishant = new Person('Nishant', 'Salhotra', '5/11/1994');
+    expect(nishant).toEqual({
+      firstName: 'Nishant',
+      lastName: 'Salhotra',
+      dateOfBirth: '5/11/1994',
+    });
+  });
+
+  test('should have a method to add the digits in date of birth', () => {
+    const nishant = new Person('Nishant', 'Salhotra', '5/11/1994');
+    expect(nishant.addDobDigits()).toBe(30);
+  });
+});
+
+describe('sequentialPromise', () => {
+  test('a', async () => {
+    const p1 = () => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('dude');
+      }, 1000);
+    });
+
+    const p2 = dude => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(`${dude}, wheres my car`);
+      }, 300);
+    });
+
+    const p3 = movieName => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(`${movieName} is a terrible movie`);
+      }, 0);
+    });
+
+    await expect(sequentialPromise([p1, p2, p3]) instanceof Promise).toBe(true);
+
+    await expect(sequentialPromise([p1, p2, p3]))
+      .resolves
+      .toBe('dude, wheres my car is a terrible movie');
+  });
+
+  test('as', async () => {
+    const p1 = () => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Godfather');
+      }, 1000);
+    });
+
+    const p2 = movieName => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(`${movieName} is an awesome movie`);
+      }, 0);
+    });
+
+    await expect(sequentialPromise([p1, p2])).resolves.toBe('Godfather is an awesome movie');
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,71 @@
+// When we call timeout, we need to get the greeting (Hello Pesto) asynchronously
+// 1 (*)
+// Would the following code work? If not, check out this function's test cases
+// and correct the function.
+function timeout(name) {
+  return setTimeout(() => `Hello ${name}`, 300);
+}
+
+// 2 (*)
+function sayHi(name) {
+  const greeting = `Hey ${name}`;
+  return greeting;
+}
+
+function getName(value) {
+  return value;
+}
+
+function greet(name) {
+  const result = getName(name).then(sayHi);
+  return result;
+}
+
+// 3 (*)
+// Pass the tests for the following function
+function rejectPromise() {
+  const promise = new Promise((resolve, reject) => {
+    const errorObject = 'Rejected';
+    setTimeout(() => reject(errorObject), 300);
+  });
+  return promise.catch(() => {});
+}
+
+// 4 (*)
+function allPromises() {
+
+}
+
+// 5 (*)
+function sequentialPromise() {
+
+}
+
+// Classes (*)
+// Implement a queue using ES6 class. See test cases for Queue
+class Queue {
+
+}
+
+// Convert the function below to ES6 class (*)
+function Person(firstName, lastName, dateOfBirth) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dateOfBirth = dateOfBirth;
+}
+
+Person.prototype.addDobDigits = function addDobDigits() {
+  return this.dateOfBirth.match(/\d/g).reduce((acc, item) => Number(acc) + Number(item));
+};
+
+module.exports = {
+  timeout,
+  greet,
+  rejectPromise,
+  allPromises,
+  Queue,
+  Person,
+  sequentialPromise,
+};
+
+// See the questions in Udacity Promise course for MCQ.
